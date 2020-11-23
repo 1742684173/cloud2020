233622fb2aa65c37d783bed15fcfc330873450ee
zxl <174@qq.com>
2020-11-23 16:24:18 +0800

    gateway

---
+++ cloud-stream-tabbitmq-provider8801/src/main/java/com/atguigu/springcloud/StreamMAMain8801.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamMAMain8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMAMain8801.class,args);
+    }
+}
